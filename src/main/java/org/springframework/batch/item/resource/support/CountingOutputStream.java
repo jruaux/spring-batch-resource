291357193d809b2084628c09d72ac06b9961ae04
jruaux <jruaux@gmail.com>
2020-11-02 15:04:42 -0800

    added bintray publishing

--- src/main/java/org/springframework/batch/item/resource/support/CountingOutputStream.java
+++ src/main/java/org/springframework/batch/item/resource/support/CountingOutputStream.java
@@ -26,41 +26,45 @@ import java.io.OutputStream;
  */
 public final class CountingOutputStream extends FilterOutputStream {
 
-	private long count;
+    private long count;
 
-	/**
-	 * Wraps another output stream, counting the number of bytes written.
-	 *
-	 * @param out the output stream to be wrapped
-	 */
-	public CountingOutputStream(OutputStream out) {
-		super(out);
-	}
+    /**
+     * Wraps another output stream, counting the number of bytes written.
+     *
+     * @param out the output stream to be wrapped
+     */
+    public CountingOutputStream(OutputStream out) {
+        super(out);
+    }
 
-	/** Returns the number of bytes written. */
-	public long getCount() {
-		return count;
-	}
+    /**
+     * 
+     * @return number of bytes written.
+     */
+    public long getCount() {
+        return count;
+    }
 
-	@Override
-	public void write(byte[] b, int off, int len) throws IOException {
-		out.write(b, off, len);
-		count += len;
-	}
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        out.write(b, off, len);
+        count += len;
+    }
 
-	@Override
-	public void write(int b) throws IOException {
-		out.write(b);
-		count++;
-	}
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+        count++;
+    }
 
-	// Overriding close() because FilterOutputStream's close() method pre-JDK8 has
-	// bad behavior:
-	// it silently ignores any exception thrown by flush(). Instead, just close the
-	// delegate stream.
-	// It should flush itself if necessary.
-	@Override
-	public void close() throws IOException {
-		out.close();
-	}
+    // Overriding close() because FilterOutputStream's close() method pre-JDK8 has
+    // bad behavior:
+    // it silently ignores any exception thrown by flush(). Instead, just close the
+    // delegate stream.
+    // It should flush itself if necessary.
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+
 }
--- src/main/java/org/springframework/batch/item/xml/XmlItemReader.java
+++ src/main/java/org/springframework/batch/item/xml/XmlItemReader.java
@@ -10,21 +10,17 @@ import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 
 /**
- * {@link ItemStreamReader} implementation that reads XML objects from a
- * {@link Resource} having the following format:
- * <p>
- * <code>
+ * {@link ItemStreamReader} implementation that reads XML objects from a {@link Resource} having the following format:
+ * 
+ * <pre>
+ * {@code
  * <root>
- *    <item>
- *       // XML object
- *    </item>
- *    <item>
- *       // XML object
- *    </item>
- * ]
+ *    <item>...</item>
+ *    <item>...</item>
  * </root>
- * <p>
- *
+ * }
+ * </pre>
+ * 
  * The implementation is <b>not</b> thread-safe.
  *
  * @param <T> the type of XML objects to read
@@ -32,83 +28,81 @@ import org.springframework.util.Assert;
  * @author Julien Ruaux
  */
 public class XmlItemReader<T> extends AbstractItemCountingItemStreamItemReader<T>
-		implements ResourceAwareItemReaderItemStream<T> {
+        implements ResourceAwareItemReaderItemStream<T> {
 
-	private static final Log LOGGER = LogFactory.getLog(XmlItemReader.class);
+    private static final Log LOGGER = LogFactory.getLog(XmlItemReader.class);
 
-	private Resource resource;
+    private Resource resource;
 
-	private XmlObjectReader<T> xmlObjectReader;
+    private XmlObjectReader<T> xmlObjectReader;
 
-	private boolean strict = true;
+    private boolean strict = true;
 
-	/**
-	 * Create a new {@link XmlItemReader} instance.
-	 * 
-	 * @param resource         the input XML resource
-	 * @param xmlObjectReader the XML object reader to use
-	 */
-	public XmlItemReader(Resource resource, XmlObjectReader<T> xmlObjectReader) {
-		Assert.notNull(resource, "The resource must not be null.");
-		Assert.notNull(xmlObjectReader, "The XML object reader must not be null.");
-		this.resource = resource;
-		this.xmlObjectReader = xmlObjectReader;
-	}
+    /**
+     * Create a new {@link XmlItemReader} instance.
+     * 
+     * @param resource the input XML resource
+     * @param xmlObjectReader the XML object reader to use
+     */
+    public XmlItemReader(Resource resource, XmlObjectReader<T> xmlObjectReader) {
+        Assert.notNull(resource, "The resource must not be null.");
+        Assert.notNull(xmlObjectReader, "The XML object reader must not be null.");
+        this.resource = resource;
+        this.xmlObjectReader = xmlObjectReader;
+    }
 
-	/**
-	 * Set the {@link XmlObjectReader} to use to read and map XML elements to
-	 * domain objects.
-	 * 
-	 * @param xmlObjectReader the XML object reader to use
-	 */
-	public void setXmlObjectReader(XmlObjectReader<T> xmlObjectReader) {
-		this.xmlObjectReader = xmlObjectReader;
-	}
+    /**
+     * Set the {@link XmlObjectReader} to use to read and map XML elements to domain objects.
+     * 
+     * @param xmlObjectReader the XML object reader to use
+     */
+    public void setXmlObjectReader(XmlObjectReader<T> xmlObjectReader) {
+        this.xmlObjectReader = xmlObjectReader;
+    }
 
-	/**
-	 * In strict mode the reader will throw an exception on
-	 * {@link #open(org.springframework.batch.item.ExecutionContext)} if the input
-	 * resource does not exist.
-	 * 
-	 * @param strict true by default
-	 */
-	public void setStrict(boolean strict) {
-		this.strict = strict;
-	}
+    /**
+     * In strict mode the reader will throw an exception on {@link #open(org.springframework.batch.item.ExecutionContext)} if
+     * the input resource does not exist.
+     * 
+     * @param strict true by default
+     */
+    public void setStrict(boolean strict) {
+        this.strict = strict;
+    }
 
-	@Override
-	public void setResource(Resource resource) {
-		this.resource = resource;
-	}
+    @Override
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
 
-	@Nullable
-	@Override
-	protected T doRead() throws Exception {
-		return xmlObjectReader.read();
-	}
+    @Nullable
+    @Override
+    protected T doRead() throws Exception {
+        return xmlObjectReader.read();
+    }
 
-	@Override
-	protected void doOpen() throws Exception {
-		if (!this.resource.exists()) {
-			if (this.strict) {
-				throw new IllegalStateException("Input resource must exist (reader is in 'strict' mode)");
-			}
-			LOGGER.warn("Input resource does not exist " + this.resource.getDescription());
-			return;
-		}
-		if (!this.resource.isReadable()) {
-			if (this.strict) {
-				throw new IllegalStateException("Input resource must be readable (reader is in 'strict' mode)");
-			}
-			LOGGER.warn("Input resource is not readable " + this.resource.getDescription());
-			return;
-		}
-		this.xmlObjectReader.open(this.resource);
-	}
+    @Override
+    protected void doOpen() throws Exception {
+        if (!this.resource.exists()) {
+            if (this.strict) {
+                throw new IllegalStateException("Input resource must exist (reader is in 'strict' mode)");
+            }
+            LOGGER.warn("Input resource does not exist " + this.resource.getDescription());
+            return;
+        }
+        if (!this.resource.isReadable()) {
+            if (this.strict) {
+                throw new IllegalStateException("Input resource must be readable (reader is in 'strict' mode)");
+            }
+            LOGGER.warn("Input resource is not readable " + this.resource.getDescription());
+            return;
+        }
+        this.xmlObjectReader.open(this.resource);
+    }
 
-	@Override
-	protected void doClose() throws Exception {
-		this.xmlObjectReader.close();
-	}
+    @Override
+    protected void doClose() throws Exception {
+        this.xmlObjectReader.close();
+    }
 
 }
